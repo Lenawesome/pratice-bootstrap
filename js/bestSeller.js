a5a6ebed9e982b87b896d593a13f8f3d600fb3ca
Lenawesome <nawilldominate@gmail.com>
2019-09-09 22:38:09 +0700

    Add section 5: New products

--- js/bestSeller.js
+++ js/bestSeller.js
@@ -1,16 +1,20 @@
 $('.owl-carousel-product').owlCarousel({
-    loop:true,
+    loop:false,
     margin:10,
     responsiveClass: true,
     responsive:{
         0:{
-            items:1
+            items: 1
         },
         600:{
+            items:2
+        },
+        800:{
             items:3
         },
-        1000:{
+        1200:{
             items:4
         }
     }
 })
+
