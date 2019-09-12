f91b5c5f16a72ae46a6a1f97fd6801efaba56807
Lenawesome <nawilldominate@gmail.com>
2019-09-12 17:13:47 +0700

    Fix animation email field

--- js/expandIcon.js
+++ js/expandIcon.js
@@ -1,11 +1,12 @@
 function expandEmail(){
-    if($(".social-link").hasClass("expanded")){
-        $(".social-link").removeClass("expanded");
+    if($(".email-wrapper").hasClass("expanded")){
         $(".expand-btn").removeClass("moved");
         $(".expand-btn").html("<i class='ti-angle-right'>");
+        $(".email-wrapper").removeClass("expanded");
     }else{
-        $(".social-link").addClass("expanded");
+        $(".email-wrapper").addClass("expanded");
         $(".expand-btn").addClass("moved");
         $(".expand-btn").html("<i class='ti-angle-left'>");
+        $(".email-wrapper").addClass("expanded");
     }
 }
